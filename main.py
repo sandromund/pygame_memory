e39220d120d4e03964b2b7404ed08f2dc74ff4f5
Sandro Mund <sandro.mund@consistec.de>
2024-01-26 11:37:44 +0100

    Added Game class

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys
 import yaml
 import json
 
+from src.game import Game
 
 CONFIG_FILE = "config.yaml"
 PORTS_FILE = "ports.json"
-
 
 with open(PORTS_FILE) as json_file:
     ports = json.load(json_file)
@@ -15,13 +15,16 @@ with open(CONFIG_FILE) as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
 pygame.init()
-screen = pygame.display.set_mode((config.get("window_size"),
-                                  config.get("window_size")))
+window_size = int(config.get("tile_size")) * int(config.get("board_size"))
+screen = pygame.display.set_mode((window_size, window_size))
 pygame.display.set_caption(config.get("game_title"))
+
+game = Game(ports=ports, config=config)
 
 running = True
 while running:
     screen.fill(tuple(config.get("background_color")))
+    game.draw_board(screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
